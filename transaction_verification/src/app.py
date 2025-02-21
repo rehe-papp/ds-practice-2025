050741592c481cc2a7d44301f90d79b159f4069f
jaredWild <jaredmichael@ut.ee>
2025-02-21 13:32:30 +0200

    adding transaction_verification

---
+++ transaction_verification/src/app.py
@@ -0,0 +1,45 @@
+import sys
+import os
+
+# This set of lines are needed to import the gRPC stubs.
+# The path of the stubs is relative to the current file, or absolute inside the container.
+# Change these lines only if strictly needed.
+FILE = __file__ if '__file__' in globals() else os.getenv("PYTHONFILE", "")
+transaction_verification_grpc_path = os.path.abspath(os.path.join(FILE, '../../../utils/pb/transaction_verification'))
+sys.path.insert(0, transaction_verification_grpc_path)
+import transaction_verification_pb2 as transaction_verification
+import transaction_verification_pb2_grpc as transaction_verification_grpc
+
+import grpc
+from concurrent import futures
+
+# Create a class to define the server functions, derived from
+# transaction_verification_pb2_grpc.HelloServiceServicer
+class HelloService(transaction_verification_grpc.HelloServiceServicer):
+    # Create an RPC function to say hello
+    def SayHello(self, request, context):
+        # Create a HelloResponse object
+        response = transaction_verification.HelloResponse()
+        # Set the greeting field of the response object
+        response.greeting = "Hello, " + request.name
+        # Print the greeting message
+        print(response.greeting)
+        # Return the response object
+        return response
+
+def serve():
+    # Create a gRPC server
+    server = grpc.server(futures.ThreadPoolExecutor())
+    # Add HelloService
+    transaction_verification_grpc.add_HelloServiceServicer_to_server(TransactionVerificationService(), server)
+    # Listen on port 50052
+    port = "50052"
+    server.add_insecure_port("[::]:" + port)
+    # Start the server
+    server.start()
+    print("Server started. Listening on port 50052.")
+    # Keep thread alive
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
